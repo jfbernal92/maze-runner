2c1a09a5422bf73cb434b16ae3954216e56d96cb
jfbernal92 <jota.efebr@hotmail.com>
2018-12-04 23:05:36 +0100

    Updated

--- client/index.js
+++ client/index.js
@@ -1,3 +1,16 @@
-import Client from "./Game";
+/*jshint esversion:6*/
+import 'phaser';
+import config from './config';
+import GameScene from './Scenes/GameScene.js';
+import BootScene from './Scenes/Boot.js';
+class Game extends Phaser.Game {
+    constructor() {
+        super(config);
+        this.scene.add('Boot', BootScene);
+        this.scene.add('GameScene', GameScene);
+        this.scene.start('Boot');
+      
+    }
+}
 
-window.game = new Client();
+window.game = new Game();
